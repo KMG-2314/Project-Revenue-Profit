dc51308b0e5b84d4d652a81c7a5b0c1ce9d52cc2
KMG-2314 <aditya.tomar@kmgin.com>
2025-10-31 13:03:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ def index():
 
 if __name__ == "__main__":
     print("✅ Flask app running at http://127.0.0.1:5000")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=False)
